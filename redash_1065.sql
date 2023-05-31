5ec947842f8b8328682f5fb942d5f44c6a7e8afa
Tran Phuoc Sang <104482029+sangtpNinjavanVN@users.noreply.github.com>
2023-05-31 23:58:07 +0700

    add service_endtime

--- redash_1065.sql
+++ redash_1065.sql
@@ -30,13 +30,14 @@ orders_cfg AS (
         ,first_value(h.hub_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub_id
         ,first_value(h.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub
         ,first_value(trim(substring(h.name,1,3))) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_province
-        ,first_value(t1.seq_no) OVER (PARTITION BY t1.order_id ORDER BY  t1.seq_no DESC) AS last_seq
+        ,max(t1.seq_no) AS last_seq
         ,first_value(t1.contact) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact
         ,first_value(t1.route_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_route
         ,first_value(route_logs.driver_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_driver
         ,first_value(t1.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_name
         ,first_value(concat(t1.address1," - ", t1.address2)) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_address
         ,first_value(transaction_failure_reason.failure_reason_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_failure_reason_id
+        ,max(t1.service_end_time) AS last_attempt_at
 
     FROM orders o force index (granular_status, primary, shipper_id)
     JOIN transactions t1 force index (order_id, service_end_time, type, seq_no, waypoint_id, route_id) ON o.id = t1.order_id
@@ -132,6 +133,7 @@ SELECT
     ,delivery_hub_id
     ,delivery_hub
     ,no_attempts
+    ,last_attempt_at + interval 7 hour AS last_attempt_at
     ,last_failure_reason_id
     ,last_route
     ,last_driver
