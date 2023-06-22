1b1e59e4e8997e4dd4668008aae0a00bc657eeb7
Tran Phuoc Sang <104482029+sangtpNinjavanVN@users.noreply.github.com>
2023-06-22 16:04:20 +0700

    replace groupby => distinct

--- redash_1065.sql
+++ redash_1065.sql
@@ -50,20 +50,21 @@ root AS (
 ),
 orders_cfg AS (
     SELECT
+        distinct
         root.*
         ,first_value(wp.latitude) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_latitude
         ,first_value(wp.longitude) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_longitude
         ,first_value(h.hub_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub_id
         ,first_value(h.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub
         ,first_value(trim(substring(h.name,1,3))) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_province
-        ,max(t1.seq_no) AS last_seq
+        ,first_value(t1.seq_no) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_seq
         ,first_value(t1.contact) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact
         ,first_value(t1.route_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_route
         ,first_value(route_logs.driver_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_driver
         ,first_value(t1.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_name
         ,first_value(concat(t1.address1," - ", t1.address2)) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_address
         ,first_value(transaction_failure_reason.failure_reason_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_failure_reason_id
-        ,max(t1.service_end_time) AS last_attempt_at
+        ,first_value(t1.service_end_time) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_seq AS last_attempt_at
 
     FROM root
 
@@ -94,7 +95,6 @@ orders_cfg AS (
     
     WHERE TRUE
     AND h.region_id = {{region}}
-    GROUP BY 1
 )
 ,pre AS (
     SELECT 
