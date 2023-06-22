85d997b123139d19e34fbf8f357c2be6c6644395
Tran Phuoc Sang <104482029+sangtpNinjavanVN@users.noreply.github.com>
2023-06-22 17:46:49 +0700

    rm hub Viet

--- redash_1105.sql
+++ redash_1105.sql
@@ -72,6 +72,7 @@ orders_cfg AS (
             AND zones_view.system_id = 'vn'
             AND hubs.system_id = 'vn'
             AND hubs.region_id = {{region}}
+            AND hubs.hub_id !=1
         ) h ON h.legacy_zone_id = wp.routing_zone_id
     LEFT JOIN route_logs force index (primary, created_at) ON  route_logs.id = t1.route_id
         AND route_logs.created_at > now() - interval 3 day
