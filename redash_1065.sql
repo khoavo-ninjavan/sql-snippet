c1c61d7b60828c089558aa087ea3a28a6d6f014c
uyentttNinjavanVN <uyen.truongthithu@ninjavan.co>
2023-07-28 22:40:39 +0700

    new update

--- redash_1065.sql
+++ redash_1065.sql
@@ -59,13 +59,13 @@ orders_cfg AS (
         ,first_value(h.hub_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub_id
         ,first_value(h.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_hub
         ,first_value(trim(substring(h.name,1,3))) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS delivery_province
-        ,first_value(t1.seq_no) OVER (PARTITION BY t1.order_id ORDER BY IF(t1.status='Fail' AND t1.service_end_time < curdate() - interval 17 hour, t1.seq_no, 0) DESC) AS last_seq
+        ,first_value(t1.seq_no) OVER (PARTITION BY t1.order_id ORDER BY IF(t1.status='Fail' AND t1.service_end_time < curdate() - interval 7 hour, t1.seq_no, 0) DESC) AS last_seq
         ,first_value(t1.contact) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact
         ,first_value(t1.route_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_route
         ,first_value(route_logs.driver_id) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_driver
         ,first_value(t1.name) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_name
         ,first_value(concat(t1.address1," - ", t1.address2)) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_contact_address
-        ,first_value(transaction_failure_reason.failure_reason_id) OVER (PARTITION BY t1.order_id ORDER BY IF(t1.status='Fail' AND t1.service_end_time < curdate() - interval 17 hour, t1.seq_no, 0) DESC) AS last_failure_reason_id
+        ,first_value(transaction_failure_reason.failure_reason_id) OVER (PARTITION BY t1.order_id ORDER BY IF(t1.status='Fail' AND t1.service_end_time < curdate() - interval 7 hour, t1.seq_no, 0) DESC) AS last_failure_reason_id
         ,first_value(t1.service_end_time) OVER (PARTITION BY t1.order_id ORDER BY t1.seq_no DESC) AS last_attempt_at
 
     FROM root
